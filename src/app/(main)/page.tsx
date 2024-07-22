7a8f2f35e8169273ffe31e21b7a46fd1213dad8c
egublog <egukei0729@gmail.com>
2024-07-22 15:15:25 +0900

    modify: page.tsxを移動して、workspacesの修正

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,15 @@
+import { getUserData } from '@/actions/get-user-data';
+import { Button } from '@/components/ui/button';
+import { redirect } from 'next/navigation';
+
+export default async function Home() {
+  const userData = await getUserData();
+
+  if (!userData) return redirect('/auth');
+
+  const userWorkspaceId = userData.workspaces?.[0];
+
+  if (!userWorkspaceId) return redirect('/create-workspace');
+
+  if (userWorkspaceId) return redirect(`/workspace/${userWorkspaceId}`);
+}
