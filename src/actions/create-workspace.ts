5ff9b031cd6d50cca0ffeff7875efc6c34a66e33
egublog <egukei0729@gmail.com>
2024-07-26 00:25:43 +0900

    add: updateWorkSpace

--- src/actions/create-workspace.ts
+++ src/actions/create-workspace.ts
@@ -1,5 +1,9 @@
 'use server';
 
+import { supabaseServerClient } from '@/supabase/supabaseServer';
+import { getUserData } from './get-user-data';
+import { updateUserWorkspace } from './update-user-workspace';
+
 export const createWorkspace = async ({
   imageUrl,
   name,
@@ -11,5 +15,36 @@ export const createWorkspace = async ({
   slug: string;
   invite_code: string;
 }) => {
-  console.log(imageUrl, name, slug, invite_code);
+  const supabase = await supabaseServerClient();
+  const userData = await getUserData();
+
+  if (!userData) {
+    return { error: 'No user data' };
+  }
+
+  const { error, data: workspaceRecord } = await supabase
+    .from('workspaces')
+    .insert({
+      image_url: imageUrl,
+      name,
+      super_admin: userData.id,
+      slug,
+      invite_code,
+    })
+    .select('*');
+
+  if (error) {
+    return { error };
+  }
+
+  const [updateWorkspaceData, updateWorkspaceError] = await updateUserWorkspace(
+    userData.id,
+    workspaceRecord[0].id,
+  );
+
+  if (updateWorkspaceError) {
+    return { error: updateWorkspaceError };
+  }
+
+  // Add user to workspace members
 };
