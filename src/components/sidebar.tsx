c97954103c2dc606788ad2a2bdf94b6753fb4b6f
egublog <egukei0729@gmail.com>
2024-08-06 01:08:44 +0900

    add: sidebarコンポーネント

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,11 @@
+import { Workspace } from '@/types/app';
+import { FC } from 'react';
+
+type SidebarProps = {
+  userWorkspacesData: Workspace[];
+};
+
+const Sidebar: FC<SidebarProps> = () => {
+  return <></>;
+};
+export default Sidebar;
