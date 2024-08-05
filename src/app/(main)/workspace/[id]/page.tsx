7b3e91b5674305d688e477d8faa49530519d99ce
egublog <egukei0729@gmail.com>
2024-08-06 01:02:22 +0900

    add: workspace/[id]のページ

--- src/app/(main)/workspace/[id]/page.tsx
+++ src/app/(main)/workspace/[id]/page.tsx
@@ -1,4 +1,16 @@
-const Workspace = () => {
+import { getUserData } from '@/actions/get-user-data';
+import { getCurrentWorkspaceData, getUserWorkspaceData } from '@/actions/workspaces';
+import { redirect } from 'next/navigation';
+
+const Workspace = async ({ params: { id } }: { params: { id: string } }) => {
+  const userData = await getUserData();
+
+  if (!userData) return redirect('/auth');
+
+  const [userWorkspaceData, useWorkspaceError] = await getUserWorkspaceData(userData.workspaces!);
+
+  const [currentWorkspaceData, currentWorkspaceError] = await getCurrentWorkspaceData(id);
+
   return (
     <div>
       <h1>Workspace</h1>
