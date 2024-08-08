e3013c7b408ce6b4a75a96153f6162c4d86cc51b
egublog <egukei0729@gmail.com>
2024-08-08 18:41:12 +0900

    add: アバターを追加

--- src/components/sidebar-nav.tsx
+++ src/components/sidebar-nav.tsx
@@ -1,18 +1,37 @@
 import { Workspace } from '@/types/app';
 import { FC } from 'react';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import Typography from './ui/typography';
 
 type SidebarNavProps = {
   userWorkspacesData: Workspace[];
   currentWorkspaceData: Workspace;
 };
 
-const SidebarNav: FC<SidebarNavProps> = () => {
+const SidebarNav: FC<SidebarNavProps> = ({ currentWorkspaceData, userWorkspacesData }) => {
   return (
     <nav>
       <ul className="flex flex-col space-y-4">
         <li>
           <div className="cursor-pointer items-center text-white mb-4 w-10 h-10 rounded-lg overflow-hidden">
-            slack
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  <Avatar>
+                    <AvatarImage
+                      src={currentWorkspaceData.image_url || ''}
+                      alt={currentWorkspaceData.name}
+                      className="object-cover w-full h-full"
+                    />
+                    <AvatarFallback className="bg-neutral-700">
+                      <Typography variant="p" text={currentWorkspaceData.name.slice(0, 2)} />
+                    </AvatarFallback>
+                  </Avatar>
+                </TooltipTrigger>
+                <TooltipContent></TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
           </div>
         </li>
       </ul>
