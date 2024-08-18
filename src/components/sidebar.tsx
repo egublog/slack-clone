28ffcd074836b343d45fee6fa54104d399b1afe5
egublog <egukei0729@gmail.com>
2024-08-18 23:45:07 +0900

    add: sidebarのprofileのtooltipやpopover

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,10 +3,13 @@
 import { FiPlus } from 'react-icons/fi';
 import { GoDot, GoDotFill } from 'react-icons/go';
 import { GiNightSleep } from 'react-icons/gi';
+import { FaRegCalendarCheck } from 'react-icons/fa';
+import { FaPencil } from 'react-icons/fa6';
+import { IoDiamondOutline } from 'react-icons/io5';
 
 import { User, Workspace } from '@/types/app';
 import SidebarNav from '@/components/sidebar-nav';
-import { Tooltip, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import Image from 'next/image';
 import { cn } from '@/lib/utils';
@@ -125,11 +128,58 @@ const Sidebar = ({ userWorkspacesData, currentWorkspaceData, userData }: Sidebar
                           </div>
                         </div>
                       </div>
+                      <div className="border group cursor-pointer mt-4 mb-2 p-1 rounded flex items-center space-x-2">
+                        <FaRegCalendarCheck className="group-hover:hidden" />
+                        <FaPencil className="hidden group-hover:block" />
+                        <Typography
+                          text="In a meeting"
+                          variant="p"
+                          className="text-xs text-gray-600"
+                        />
+                      </div>
+                      <div className="flex flex-col space-y-1">
+                        <Typography
+                          text={
+                            userData.is_away ? 'Set yourself as active' : 'Set yourself as away'
+                          }
+                          className="hover:text-white hover:bg-blue-700 px-2 py-1 rounded cursor-pointer"
+                          variant="p"
+                        />
+                        <Typography
+                          text="Clear Status"
+                          className="hover:text-white hover:bg-blue-700 px-2 py-1 rounded cursor-pointer"
+                          variant="p"
+                        />
+                        <hr className="bg-gray-400" />
+                        <Typography
+                          text="Profile"
+                          className="hover:text-white hover:bg-blue-700 px-2 py-1 rounded cursor-pointer"
+                          variant="p"
+                        />
+                        {/* Preferences Dialog */}
+                        <hr className="bg-gray-400" />
+                        <div className="flex gap-2 items-center hover:text-white hover:bg-blue-700 px-2 py-1 rounded cursor-pointer">
+                          <IoDiamondOutline className="text-orange-400" />
+                          <Typography
+                            text={`Upgrade ${currentWorkspaceData.name}`}
+                            className="text-xs"
+                            variant="p"
+                          />
+                        </div>
+                        <Typography
+                          text={`Sign out of ${currentWorkspaceData.name}`}
+                          className="hover:text-white hover:bg-blue-700 px-2 py-1 rounded cursor-pointer"
+                          variant="p"
+                        />
+                      </div>
                     </div>
                   </PopoverContent>
                 </Popover>
               </div>
             </TooltipTrigger>
+            <TooltipContent className="text-white bg-black" side="right">
+              <Typography text={userData.name || userData.email} variant="p" />
+            </TooltipContent>
           </Tooltip>
         </TooltipProvider>
       </div>
