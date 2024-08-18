19184d9f1785cc8b9a4f10caa53844f074a7661b
egublog <egukei0729@gmail.com>
2024-08-18 22:53:26 +0900

    add: sidebarの下部

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,5 +1,9 @@
 import { User, Workspace } from '@/types/app';
 import SidebarNav from './sidebar-nav';
+import { FiPlus } from 'react-icons/fi';
+import { Tooltip, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+import { Popover } from '@/components/ui/popover';
+import Image from 'next/image';
 
 type SidebarProps = {
   userWorkspacesData: Workspace[];
@@ -32,6 +36,44 @@ const Sidebar = ({ userWorkspacesData, currentWorkspaceData, userData }: Sidebar
         currentWorkspaceData={currentWorkspaceData}
         userWorkspacesData={userWorkspacesData}
       />
+
+      <div className="flex flex-col space-y-3">
+        <div
+          className={`
+          bg-[rgba(255,255,255,0.3)]
+          cursor-pointer
+          transition-all
+          duration-300
+          hover:scale-110
+          text-white
+          grid place-content-center rounded-full w-10 h-10
+          `}
+        >
+          <FiPlus size={28} />
+        </div>
+
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <div>
+                <Popover>
+                  <div className="h-10 w-10 relative cursor-pointer">
+                    <div className="h-full w-full rounded-lg overflow-hidden">
+                      <Image
+                        className="object-cover w-full h-full"
+                        src={userData.avatar_url}
+                        alt={userData.name || 'user'}
+                        width={300}
+                        height={300}
+                      />
+                    </div>
+                  </div>
+                </Popover>
+              </div>
+            </TooltipTrigger>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
     </aside>
   );
 };
