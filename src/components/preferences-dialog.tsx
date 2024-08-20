af5fa1c5b54895d645645a790d8da371ffb09f80
egublog <egukei0729@gmail.com>
2024-08-20 22:49:24 +0900

    add: lightモード

--- src/components/preferences-dialog.tsx
+++ src/components/preferences-dialog.tsx
@@ -2,6 +2,11 @@ import { useColorPreferences } from '@/providers/color-preferences';
 import { useTheme } from 'next-themes';
 import { Dialog, DialogContent, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import Typography from './ui/typography';
+import { HiOutlinePaintBrush } from 'react-icons/hi2';
+import { MdLightMode } from 'react-icons/md';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Button } from './ui/button';
+import { cn } from '@/lib/utils';
 
 const PreferencesDialog = () => {
   const { setTheme, theme } = useTheme();
@@ -22,6 +27,32 @@ const PreferencesDialog = () => {
           <Typography text="Preferences" variant="h3" className="py-5" />
           <hr className="bg-gray-200" />
         </DialogTitle>
+        <Tabs orientation="horizontal" defaultValue="themes">
+          <TabsList>
+            <TabsTrigger value="themes">
+              <HiOutlinePaintBrush className="mr-2" />
+              <Typography text="Themes" variant="p" />
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent className="max-w-xs md:max-w-fit" value="themes">
+            <Typography text="Color Mode" variant="p" className="py-2 font-bold" />
+            <Typography
+              text="Choose if slack-clone appearance should be light or dark, or follow the computer settings"
+              variant="p"
+              className="pb-4"
+            />
+            <div className="flex flex-wrap gap-3">
+              <Button
+                variant="outline"
+                onClick={() => setTheme('light')}
+                className={`w-full ${cn(theme === 'light' && 'border-blue-600')}`}
+              >
+                <MdLightMode className="mr-2" size={20} />
+                <Typography text="Light" variant="p" />
+              </Button>
+            </div>
+          </TabsContent>
+        </Tabs>
       </DialogContent>
     </Dialog>
   );
