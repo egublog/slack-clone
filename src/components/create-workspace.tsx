b4af2e3841c09df7084c5fe42ddc12719ac7c4cf
egublog <egukei0729@gmail.com>
2024-08-24 01:54:19 +0900

    add: CreateWorkspceの中のDialog

--- src/components/create-workspace.tsx
+++ src/components/create-workspace.tsx
@@ -1,15 +1,33 @@
 import { Button } from '@/components/ui/button';
 import { FaPlus } from 'react-icons/fa';
 import Typography from '@/components/ui/typography';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 
 const CreateWorkspace = () => {
   return (
-    <div className="flex items-center gap-2 p-2">
-      <Button variant="secondary">
-        <FaPlus />
-      </Button>
-      <Typography variant="p" text="Add Workspace" />
-    </div>
+    <Dialog>
+      <DialogTrigger>
+        <div className="flex items-center gap-2 p-2">
+          <Button variant="secondary">
+            <FaPlus />
+          </Button>
+          <Typography variant="p" text="Add Workspace" />
+        </div>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="my-4">
+            <Typography variant="p" text="Create workspace" />
+          </DialogTitle>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 };
 
