67abcf1843f7fef73dbecf6eec2c75e2c18473dd
egublog <egukei0729@gmail.com>
2024-08-27 23:06:22 +0900

    refactor: create-workspace-values.tsのコメント

--- src/hooks/create-workspace-values.ts
+++ src/hooks/create-workspace-values.ts
@@ -1,19 +1,22 @@
 import { create } from 'zustand';
 
 type CreateWorkSpaceValues = {
-  name: string;
-  imageUrl: string;
-  updateImageUrl: (url: string) => void;
-  updateValues: (values: Partial<CreateWorkSpaceValues>) => void;
-  currentStep: number;
-  setCurrentStep: (step: number) => void;
+  name: string; // ワークスペースの名前
+  imageUrl: string; // ワークスペースの画像URL
+  updateImageUrl: (url: string) => void; // 画像URLを更新する関数
+  updateValues: (values: Partial<CreateWorkSpaceValues>) => void; // 複数の値を更新する関数
+  currentStep: number; // 現在のステップ
+  setCurrentStep: (step: number) => void; // ステップを更新する関数
 };
 
+/**
+ * ワークスペースの値を管理するためのカスタムフック
+ */
 export const useCreateWorkspaceValues = create<CreateWorkSpaceValues>((set) => ({
-  name: '',
-  imageUrl: '',
-  updateImageUrl: (url) => set({ imageUrl: url }),
-  updateValues: (values) => set(values),
-  currentStep: 1,
-  setCurrentStep: (step) => set({ currentStep: step }),
+  name: '', // 初期値は空文字
+  imageUrl: '', // 初期値は空文字
+  updateImageUrl: (url) => set({ imageUrl: url }), // 画像URLを更新
+  updateValues: (values) => set(values), // 複数の値を更新
+  currentStep: 1, // 初期ステップは1
+  setCurrentStep: (step) => set({ currentStep: step }), // ステップを更新
 }));
