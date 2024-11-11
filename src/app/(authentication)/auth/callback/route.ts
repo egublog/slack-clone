84417c44239b862a89c362fab160e9e9898616c6
egublog <egukei0729@gmail.com>
2024-11-11 23:50:51 +0900

    add: createServerClientのコメント

--- src/app/(authentication)/auth/callback/route.ts
+++ src/app/(authentication)/auth/callback/route.ts
@@ -10,6 +10,7 @@ export async function GET(request: Request) {
 
   if (code) {
     const cookieStore = cookies();
+    // NOTE: createServerClient()はdeprecatedなので、代わりにcreateClient()を使う
     const supabase = createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
